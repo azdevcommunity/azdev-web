76e18e0a089b6926b6bbb80705a547e0a62b8b6f
Yusif Hasanov <maqsudsafin@gmail.com>
2024-01-04 17:25:04 +0400

    :balloon:

--- src/features/container/home/DeveloperSection.tsx
+++ src/features/container/home/DeveloperSection.tsx
@@ -3,11 +3,12 @@ import Button from "../../components/home/Button";
 const DeveloperSection = () => {
     return (
         <>
-            <div className={"developerSection text-gray-100 text-8xl font-bold ml-24  mt-20"}>
+            <div className={"developerSection text-gray-100 text-8xl font-bold ml-24 mt-20"}>
                 <div style={{lineHeight:"0.5"}}>Wanna to join us</div>
                 <div className={"headerTextBg"} ><span className={"text-white"}>as a </span>develpero<span className={"text-white"}>?</span></div>
+                <Button onClick={() => { }} >Join Community</Button>
             </div>
-            <Button onClick={() => { }} >Join Community</Button>
+            
         </>
     );
 };
--- src/features/pages/home/HomePage.tsx
+++ src/features/pages/home/HomePage.tsx
@@ -1,12 +1,14 @@
 import NavBar from "../../container/home/NavBar.tsx";
 import DeveloperSection from "../../container/home/DeveloperSection.tsx";
 import './../../styles/home.css'
+import AboutUsSection from "../../container/home/AboutUsSection.tsx";
 
 const HomePage = () => {
     return (
         <div  className={"homeContainer"}>
             <NavBar/>
             <DeveloperSection/>
+            <AboutUsSection/>
         </div>
     );
 };
