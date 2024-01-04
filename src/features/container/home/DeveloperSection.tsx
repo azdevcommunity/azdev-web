a299db94f14767bbe9315c3d6624017c7894b850
Yusif Hasanov <maqsudsafin@gmail.com>
2024-01-04 18:06:12 +0400

    :aquarius:

--- src/features/container/home/DeveloperSection.tsx
+++ src/features/container/home/DeveloperSection.tsx
@@ -3,10 +3,11 @@ import Button from "../../components/home/Button";
 const DeveloperSection = () => {
     return (
         <>
-            <div className={"developerSection text-gray-100 text-8xl font-bold ml-24 mt-20"}>
-                <div style={{lineHeight:"0.5"}}>Wanna to join us</div>
-                <div className={"headerTextBg"} ><span className={"text-white"}>as a </span>develpero<span className={"text-white"}>?</span></div>
-                <Button onClick={() => { }} >Join Community</Button>
+            <div className={"developerSection text-gray-100 text-8xl font-bold ml-24  mt-20"}>
+                <div className={"headerTextBg"} ><span className={"text-white"}>Bizə </span>develpero</div>
+                <div style={{lineHeight:"0.5"}}>olaraq qoşulmaq </div>
+                <div style={{lineHeight:"1.1"}}>istəyirsən? </div>
+                <Button onClick={() => { }} >Qatıl</Button>
             </div>
             
         </>
