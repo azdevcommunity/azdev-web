0faa579e6ded311255083f10691d1b5d335aedab
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-01-06 18:56:55 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/root/App.tsx'
-import '@/index.css'
-import { register } from '@/serviceWorkerRegistration.ts'
+
+import './index.css'
+import App from "./root/App.tsx";
+import {register} from "./serviceWorkerRegistration.ts";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
