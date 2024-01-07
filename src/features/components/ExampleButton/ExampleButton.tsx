48c74b884083584910bbc8c60abb54596c852b1a
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-01-07 18:30:54 +0400

    Create ExampleButton.tsx

---
+++ src/features/components/ExampleButton/ExampleButton.tsx
@@ -0,0 +1,13 @@
+import styles from './exampleButton.module.css'
+
+// maximum tailwind ile stylelari qur lazm olduqca css yaz
+
+const ExampleButton = () => {
+    return (
+        <div className={`${styles.exampleButton}`}>
+
+        </div>
+    );
+};
+
+export default ExampleButton;
