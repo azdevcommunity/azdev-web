06c972305a68b677418982a29dcb17272e73ddba
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-01-09 18:59:17 +0400

    Create GithubSignInButton.tsx

---
+++ src/features/container/home/component/GithubSignInButton/GithubSignInButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './GithubSignInButton.css';
+
+interface GithubSignInButtonProps {
+  onClick: () => void; // Define other props as needed
+}
+
+const GithubSignInButton: React.FC<GithubSignInButtonProps> = ({ onClick }) => {
+  return (
+    <button className="github-sign-in-button" onClick={onClick}>
+      <img src="../../../../../../public/assets/home/github32.png" className="github-logo" />
+      Sign in with Github
+    </button>
+  );
+};
+
+export default GithubSignInButton;
