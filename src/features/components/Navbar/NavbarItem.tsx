900f54591f52d242c9c96b4919627947ef000a9c
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-01-09 19:00:18 +0400

    Update NavbarItem.tsx

--- src/features/components/Navbar/NavbarItem.tsx
+++ src/features/components/Navbar/NavbarItem.tsx
@@ -1,29 +1,24 @@
-import { FC, ReactNode } from "react";
-import style from '../../styles/home.module.css';
+import {FC} from "react";
+import style from './navbar.module.css';
+import {NavbarItemProps} from "./navbar.type";
 
-interface Props {
-    children: ReactNode;
-    path?: string;
-    onClick?: () => void;
-    id?: string;
-}
 
-const NavbarItem: FC<Props> = ({ children, path, onClick ,id}) => {
+const NavbarItem: FC<NavbarItemProps> = ({children, path, onClick, id}) => {
 
     const content = path
-    ? (
-        <a href={path} className={`${style.navbarItem}`}>
-            {children}
-        </a>
-    ) : id ? ( 
-        <div className={`${style.joinUsBtn} ${id}`}>
-            {children}
-        </div>
-    ) : (
-        <div onClick={onClick} className={`${style.navbarItemButton}`}>
-            {children}
-        </div>
-    );
+        ? (
+            <a href={path} className={`${style.navbarItem}`}>
+                {children}
+            </a>
+        ) : id ? (
+            <div onClick={onClick} className={`${style.joinUsBtn} ${id}`}>
+                {children}
+            </div>
+        ) : (
+            <div onClick={onClick} className={`${style.navbarItemButton}`}>
+                {children}
+            </div>
+        );
 
     return (
         <div className={`block uppercase cursor-pointer text-gray-200`}>
