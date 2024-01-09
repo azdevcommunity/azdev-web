8fa470c968c3533901df3bbaf68c6e5ec2887718
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-01-09 18:59:23 +0400

    Create GoogleSignInButton.tsx

---
+++ src/features/container/home/component/GoogleSignInButton/GoogleSignInButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './GoogleSignInButton.css';
+
+interface GoogleSignInButtonProps {
+  onClick: () => void; // Define other props as needed
+}
+
+const GoogleSignInButton: React.FC<GoogleSignInButtonProps> = ({ onClick }) => {
+  return (
+    <button className="google-sign-in-button" onClick={onClick}>
+      <img src="../../../../../../public/assets/home/google32.png" className="google-logo" />
+      Sign in with Google
+    </button>
+  );
+};
+
+export default GoogleSignInButton;
