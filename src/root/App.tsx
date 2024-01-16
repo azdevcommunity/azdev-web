2a833803db4f16801ba97a4d426535fec06ef36e
Maqsud Safin <maqsudsafin@gmail.com>
2024-01-16 19:12:00 +0400

    Update App.tsx

--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,10 +1,7 @@
 import AppProvider from "./AppProvider"
 import {Route, Routes} from "react-router-dom";
-import HomeLayout from "../features/layout/home/HomeLayout.tsx";
 import HomePage from "../features/pages/home/HomePage.tsx";
-import LoginPage from "../features/pages/login/LoginPage.tsx";
 import DashBoard from "./DashBoard.tsx";
-import RegisterPage from "../features/pages/register/RegisterPage.tsx";
 
 
 const App = () => (
