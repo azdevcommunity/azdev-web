cf3d771e6723fc50075b0bd94b26c8af9dab28b8
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-01-17 14:52:40 +0400

    Update App.tsx

--- src/root/App.tsx
+++ src/root/App.tsx
@@ -3,16 +3,18 @@ import {Route, Routes} from "react-router-dom";
 import HomePage from "../features/pages/home/HomePage.tsx";
 import DashBoard from "./DashBoard.tsx";
 
+const App = () => {
 
-const App = () => (
-    <DashBoard>
-        <AppProvider>
-            <Routes>
-                <Route path={"/"} element={<HomePage/>}/>
-            </Routes>
-        </AppProvider>
-    </DashBoard>
-);
+    return (
+        <DashBoard>
+            <AppProvider>
+                <Routes>
+                    <Route path={"/"} element={<HomePage/>}/>
+                </Routes>
+            </AppProvider>
+        </DashBoard>
+    )
+}
 
 
 export default App
