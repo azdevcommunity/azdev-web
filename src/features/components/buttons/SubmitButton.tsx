c9a697f3fb90869a593de48d5922f1c159436d8a
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-01-17 12:03:09 +0400

    Update SubmitButton.tsx

--- src/features/components/buttons/SubmitButton.tsx
+++ src/features/components/buttons/SubmitButton.tsx
@@ -2,7 +2,7 @@ import style from "./submitbutton.module.css";
 import {FC} from "react";
 
 
-const SubmitButton: FC<Props> = ({children, buttonType = "submit"}) => {
+const SubmitButton: FC<InputGroupProps> = ({children, buttonType = "submit"}) => {
     return (
         <button type={buttonType} className={style.submitButton}>{children}</button>
     );
