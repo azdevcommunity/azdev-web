d970192986f010548cd840d27f641ca2717680f3
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-01-18 12:27:41 +0400

    Create CustomButton.tsx

---
+++ src/features/container/signin/component/CustomBlueButton/CustomButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './CustomButton.css';
+
+interface CustomButtonProps {
+    onClick?: () => void;
+    className?: string;
+    value: string;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({onClick, className = "", value }) => {
+    return (
+        <button
+            className={`custom-blue-button ${className}`} onClick={onClick}>
+            {value}
+
+        </button>
+    );
+};
+
+export default CustomButton;
