8a17df2b8f06e183d652fe843281559215dd424e
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-01-18 18:13:30 +0400

    Update App.tsx

--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,18 +1,20 @@
 import AppProvider from "./AppProvider"
 import {Route, Routes} from "react-router-dom";
-import SignIn from "../features/pages/home/SignIn.tsx";
-import DashBoard from "./DashBoard.tsx";
+import SignIn from "../features/pages/signin/SignIn.tsx";
+import MainLayout from "../features/layout/main/MainLayout.tsx";
+import HomePage from "../features/pages/home/HomePage.tsx";
 
 const App = () => {
 
     return (
-        <DashBoard>
-            <AppProvider>
-                <Routes>
-                    <Route path={"/"} element={<SignIn/>}/>
-                </Routes>
-            </AppProvider>
-        </DashBoard>
+        <AppProvider>
+            <Routes>
+                <Route path={"/"} element={<SignIn/>}/>
+                <Route path={"/home"} element={<MainLayout/>}>
+                    <Route index element={<HomePage/>}/>
+                </Route>
+            </Routes>
+        </AppProvider>
     )
 }
 
