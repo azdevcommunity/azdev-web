62b172a5b14380c15e61527fb1d053c3783b86ce
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-01-20 17:03:57 +0400

    Update TrendsAndSearch.tsx

--- src/features/container/home/trendsandsearch/TrendsAndSearch.tsx
+++ src/features/container/home/trendsandsearch/TrendsAndSearch.tsx
@@ -1,6 +1,7 @@
 import SearchBar from "../../../components/searchbar/SearchBar.tsx";
-import Trends from "./Trends.tsx";
 import style from './trendsandsearch.module.css'
+import Trends from "./component/Trends.tsx";
+
 const TrendsAndSearch = () => {
     return (
         <div className={style.trendsAndSearchContainer}>
