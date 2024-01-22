b12b9481177a711f6a085f09235563164e6df7b1
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-01-22 18:36:39 +0400

    Create PostAvatar.tsx

---
+++ src/features/components/post/PostAvatar.tsx
@@ -0,0 +1,11 @@
+import {Avatar} from "@nextui-org/react";
+
+const PostAvatar = () => {
+    return (
+        <div className={"pl-2 pr-4"}>
+            <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d"/>
+        </div>
+    );
+};
+
+export default PostAvatar;
